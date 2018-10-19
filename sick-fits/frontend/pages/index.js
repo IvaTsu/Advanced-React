f36f5557aba6daea0d0c394aa06ef47491832706
IvaTsu <ivan.tsud@gmail.com>
2018-10-19 14:12:03 +0300

    Remove links

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -3,8 +3,5 @@ import Link from "next/link";
 export default () => (
   <div>
     <h1>Hello World!</h1>
-    <Link href="/sell">
-      <a>Go to Sell</a>
-    </Link>
   </div>
 );
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -3,8 +3,5 @@ import Link from "next/link";
 export default () => (
   <div>
     <h1>Sell!</h1>
-    <Link href="/">
-      <a>Go Home</a>
-    </Link>
   </div>
 );
