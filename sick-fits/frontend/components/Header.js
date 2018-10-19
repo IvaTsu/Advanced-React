26156f43645c93417d24b90e824fa73b866e1fc8
IvaTsu <ivan.tsud@gmail.com>
2018-10-19 14:12:30 +0300

    Setup links in a 'Header.js'

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,10 @@
+import Nav from "./Nav";
+
 export default () => (
   <div>
     <div className="bar">
       <a href="#">Sick Fits</a>
+      <Nav />
     </div>
     <div className="sub-bar">
       <p>Search</p>
---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <Link href="/">
+      <a>Go Home</a>
+    </Link>
+    <Link href="/sell">
+      <a>Go to Sell</a>
+    </Link>
+  </div>
+);
