7c6a18cb6a2ad7d85f6a73f834b0903c25ebb7a7
IvaTsu <ivan.tsud@gmail.com>
2018-10-22 16:41:18 +0300

    Import Theme and wrap component with 'ThemeProvider'

--- sick-fits/frontend/components/Page/index.js
+++ sick-fits/frontend/components/Page/index.js
@@ -1,11 +1,15 @@
 import Header from "../Header";
 import Meta from "../Meta";
+import { ThemeProvider } from "styled-components";
 import { StyledPage, StyledPageInnerDiv } from "./styles";
+import Theme from "./../styles/Theme";
 
 export default ({ children }) => (
-  <StyledPage>
-    <Meta />
-    <Header />
-    <StyledPageInnerDiv>{children}</StyledPageInnerDiv>
-  </StyledPage>
+  <ThemeProvider theme={Theme}>
+    <StyledPage>
+      <Meta />
+      <Header />
+      <StyledPageInnerDiv>{children}</StyledPageInnerDiv>
+    </StyledPage>
+  </ThemeProvider>
 );
