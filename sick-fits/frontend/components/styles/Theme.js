487c05aab6f0beadf000e95b78ca9c45d2426c84
IvaTsu <ivan.tsud@gmail.com>
2018-10-24 21:28:30 +0300

    Add global styling

--- sick-fits/frontend/components/styles/Theme.js
+++ sick-fits/frontend/components/styles/Theme.js
@@ -1,5 +1,16 @@
 import { injectGlobal } from "styled-components";
 
+const globalTheme = {
+  white: "#FFFFFF",
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightGrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  boxShadow: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
 injectGlobal`
   @font-face {
     font-family: "radnika-next";
@@ -20,18 +31,11 @@ injectGlobal`
     font-size: 1.6rem;
     line-height: 2;
     font-family: "radnika-next";
-  } 
+  }
+  a {
+    text-decoration: none;
+    color: ${globalTheme.black};
+  }
 `;
-
-const globalTheme = {
-  white: "#FFFFFF",
-  red: "#FF0000",
-  black: "#393939",
-  grey: "#3A3A3A",
-  lightGrey: "#E1E1E1",
-  offWhite: "#EDEDED",
-  maxWidth: "1000px",
-  boxShadow: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
-};
 
 export default globalTheme;
