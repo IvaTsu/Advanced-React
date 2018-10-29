0613d29575da8bfbce858814f09bcba603cdfe19
IvaTsu <ivan.tsud@gmail.com>
2018-10-30 00:32:29 +0200

    Init server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require("dotenv").config();
+
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO: Use Express.js middleware to handle cookies (JWT)
+// TODO: Use Express.js middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  callback => {
+    console.log(`Server started on https://localhost:${callback.port}`);
+  }
+);
